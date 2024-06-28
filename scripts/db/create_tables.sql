cf273aef845254913af0d1213635c1a0566e0e70
deku scrub <@.@>
2024-06-28 08:27:35 +0000

    Add definitions table

--- scripts/db/create_tables.sql
+++ scripts/db/create_tables.sql
@@ -6,14 +6,23 @@ CREATE TABLE IF NOT EXISTS Pronunciation (
     primary key (word, pronunciation)
 );
 
+
 CREATE TABLE IF NOT EXISTS PartOfSpeech (
     word TEXT,
     pos TEXT,
     PRIMARY KEY (word, pos)
 );
 
+
 CREATE TABLE IF NOT EXISTS Synonym (
     word1 TEXT,
     word2 TEXT,
     PRIMARY KEY (word1, word2)
+);
+
+
+CREATE VIRTUAL TABLE IF NOT EXISTS Definition USING fts5 (
+    word UNINDEXED,
+    definition,
+    tokenize = 'trigram case_sensitive 0 remove_diacritics 1'
 );
